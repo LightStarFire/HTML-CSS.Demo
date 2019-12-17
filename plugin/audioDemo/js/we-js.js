06db4e17bb7915c32b883eef95f8feb712602768
zhl <861805366@qq.com>
2019-12-17 18:21:37 +0800

    完善插件

--- plugin/audioDemo/js/we-js.js
+++ plugin/audioDemo/js/we-js.js
@@ -2,9 +2,27 @@
     // 开启严格模式，规范代码，提高浏览器运行效率
     "use strict";
 
+    // this
+    var that;
+
+    // 默认参数
+    var defaultOpts = {
+        musicList: [], // 音乐列表
+        playMode: 'order', // single(单曲循环) order(顺序播放) random(随机播放)
+    };
+
     // 定义一个音频WeAudio类，通常首字母大写
-    var WeAudio = function(el) {
+    var WeAudio = function(el, opts) {
+        Object.assign(defaultOpts, opts);
+        console.log(defaultOpts);
+        that = this;
         this.el = typeof el === 'string' ? document.querySelector(el) : el;
+        init();
+    };
+
+    // weAudio status 音频的一些状态
+    var status = {
+        isNotEmptyArray: true
     };
 
     // 定义一个正在播放的音乐类WeActiveMusic
@@ -18,14 +36,68 @@
 
     // 覆写原型链，给继承者提供方法
     WeAudio.prototype = {
-        init: function() {
-            this.el.innerHTML = addWeAudioDom();
+        // init: function() {
+        //     this.el.innerHTML = addWeAudioDom();
+        // }
+    };
+
+    // 播放器初始化
+    function init () {
+        // 参数检查
+        checkParams();
+        // 根据模式选择一首歌曲
+        playMusicForMode();
+        // 插入节点
+        that.el.innerHTML = addWeAudioDom();
+        // 
+    };
+
+    var utils = {
+        // 不是一个空的数组 true 反之 false
+        isNotEmptyArray: function (_arr) {
+            if (!Array.isArray(_arr)) {
+                return false;
+            }
+            if (_arr.length === 0) {
+                return false;
+            }
+            return true;
         }
     };
 
-    // 插入节点
+    // 参数检查
+    function checkParams () {
+        status.isNotEmptyArray = utils.isNotEmptyArray(defaultOpts.musicList);
+        if (!status.isNotEmptyArray) {
+            return;
+        }
+        // defaultOpts.musicList.length
+    };
+
+    // 根据模式选择一首歌曲
+    function playMusicForMode () {
+        switch (defaultOpts.playMode) {
+            case 'single':
+            // 单曲循环
+
+            break;
+            case 'order':
+            // 顺序播放
+            break;
+            case 'random':
+            // random
+            break;
+        }
+    };
+
+    // 单曲循环
+    
+    
+    // 插入节点 配置皮肤什么的
     function addWeAudioDom () {
-        var audioDom = '<div class="component-we-audio">' +
+        var audioDom;
+        if (!status.isNotEmptyArray) {
+            audioDom = '<div class="component-we-audio">' +
                             '<div class="we-audio we-clearfix">' +
                             '<div class="we-song-info we-clearfix">' +
                                 '<img src="./images/songer.jpg" class="we-song-cover we-fl">' +
@@ -43,11 +115,14 @@
                                 '</div>' +
                             '</div>' +
                             '</div>' +
-                            '<audio :src="activeMusic.songSrc" ref="weAudio">' +
+                            '<audio id="weAudio">' +
                             '</audio>' +
                         '</div>';
-                        return audioDom;
-    }
+        } else {
+            audioDom = '<div class="component-we-audio"><div class="we-audio we-clearfix"><p>噢~你这该死的家伙!歌曲资源都弄错了。</p></div></div>'
+        }
+        return audioDom;
+    };
 
     // 兼容CommonJs规范
     if (typeof module !== 'undefined' && module.exports) module.exports = WeAudio;
